08591cfd5b37a7750958fde4acdc4a8aad43ba82
Houtarchat <admin@houtarchat.ml>
2023-11-22 14:13:04 +0800

    修改示例代码

--- examples/6gxs.net.js
+++ examples/6gxs.net.js
@@ -1,7 +1,8 @@
+/// <reference lib="es2021" />
 const reader = new Shosetsu({
     selector: {
         title: "#container > div > div > div.reader-main > h1",
-        readContentSet: "#container",
+        readContentSet: "body",
         readDetail: "#container > div > div > div.reader-main",
         read_chapterDetail: "#content",
         removeList: [
@@ -15,23 +16,52 @@ const reader = new Shosetsu({
             "body > ul",
             "#container > div > div > div.layout-tit.xs-hidden"
         ],
-        pageNav: "#container > div > div > div.reader-main > div.section-opt.m-bottom-opt"
+        pageNav: "div.section-opt.m-bottom-opt"
+
     },
     function: {
         getNextPage: function () {
             return nextpage;
         },
         getChapterDetail: function (nextPageDoc) {
-            const content = nextPageDoc.querySelector("#content");
-            content.innerHTML = $.base64.decode(content.innerHTML.trim())
-                .replaceAll(/<img src=["']http:\/\/www\.6gxs\.net\/img\/dh\.png["'] height=["']26["'] ?\/?>/g, "，")
-                .replaceAll(/<img src=["']http:\/\/www\.6gxs\.net\/img\/wenhao\.png["'] height=["']26["'] ?\/?>/g, "？")
-                .replaceAll(/<img src=["']http:\/\/www\.6gxs\.net\/img\/hao1\.png["'] height=["']26["'] ?\/?>/g, "”")
-                .replaceAll(/<img src=["']http:\/\/www\.6gxs\.net\/img\/hao2\.png["'] height=["']26["'] ?\/?>/g, "！")
-                .replaceAll(/<img src=["']http:\/\/www\.6gxs\.net\/img\/hao3\.png["'] height=["']26["'] ?\/?>/g, "。");
-            content.style.display = "block";
-            window.nextpage = nextPageDoc.querySelector("script:not([src])").textContent.match(/var nextpage = '(.*)'/)[1];
-            return content;
+            const contentElement = nextPageDoc.querySelector("#content");
+            contentElement.innerHTML = $.base64.decode(contentElement.innerHTML.trim());
+            [
+                ["dh", "，"],
+                ["wenhao", "？"],
+                ["hao1", "”"],
+                ["hao2", "！"],
+                ["hao3", "。"]
+            ].forEach(([imgSrc, replacement]) => {
+                const regexPattern = new RegExp(`<img src=["']http://www.6gxs.net/img/${imgSrc}.png["'] height=["']26["'] ?/?>`, 'g');
+                contentElement.innerHTML = contentElement.innerHTML.replaceAll(regexPattern, replacement);
+            });
+
+            contentElement.innerHTML = contentElement.innerHTML
+                .split(/<br ?\/?>/)
+                .map((segment) => {
+                    return `<p>${segment
+                        .trim()
+                        .split('            ')
+                        .filter(
+                            (subsegment) =>
+                                subsegment.trim().length > 0 &&
+                                !['6gxs.net', '6G小说网'].some(s => subsegment.includes(s))
+                        )
+                        .join(' ')}</p>`;
+                })
+                .filter((segment) => segment.trim().length > 7)
+                .join('\n');
+
+            contentElement.style.display = "block";
+
+            window.nextpage = Array.from(
+                nextPageDoc.querySelectorAll("script:not([src])")
+            )
+                .map(e => e.textContent).join()
+                .match(/var nextpage = '(.*)'/)[1];
+
+            return contentElement;
         }
     },
     encoding: 'utf-8'
